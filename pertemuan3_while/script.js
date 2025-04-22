19693c9b88d088de764300ee330e7bc3a5309af5
ikbarrahma <ikbarrahma@gmail.com>
2025-04-22 20:10:54 +0700

    Mannambahkan pertemuan 3 dan 4

---
+++ pertemuan3_while/script.js
@@ -0,0 +1,19 @@
+// Membuat PopUp Box
+// Ikbar rahma
+
+// looping yang di hentikan oleh user
+
+// var user = true;
+// while (user) {
+//   var nama = prompt("Masukan namamu?");
+//   alert("Nama saya adalah " + nama);
+//   user = confirm("Ingin coba lagi?");
+// }
+
+// Looping yang di hentikan oleh sistem
+
+// var i = 1;
+// while (i <= 5) {
+//   console.log(i);
+//   i++;
+// }
---
+++ pertemuan4_melanjutkan_kasus_angkot/angkot1.js
@@ -0,0 +1,10 @@
+// studi kasus angkot
+// ikbar rahma
+
+var jumlahAngkot = 10;
+var noAngkot = 1;
+
+while (noAngkot <= jumlahAngkot) {
+  console.log("Angkot No. " + noAngkot + " beroprasi dengan baik.");
+  noAngkot++;
+}
