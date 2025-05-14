69fc21bcf20ef9cf6f2d07d12522f86e699e9890
ikbarrahma <ikbarrahma@gmail.com>
2025-05-14 21:03:52 +0700

    menambahkan perulangan switch case

---
+++ pertemuan9_switch_case/script.js
@@ -0,0 +1,19 @@
+var angka = prompt("Masukkan angka");
+
+switch (angka) {
+  case "1":
+    alert("Anda memasukkan angka " + angka);
+    break;
+  case "2":
+    alert("Anda memasukkan angka " + angka);
+    break;
+  case "3":
+    alert("Anda memasukkan angka " + angka);
+    break;
+  case "4":
+    alert("Anda memasukkan angka " + angka);
+    break;
+  case "5":
+    alert("Anda memasukkan angka " + angka);
+    break;
+}
